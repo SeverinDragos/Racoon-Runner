2b1728fc27fc17b6c41cc7d8b196c48704214579
andrei2d <andrei.tudorache100@gmail.com>
2020-03-24 21:42:26 +0200

    Created reward and perk sample objects

---
+++ Assets/Scripts/ReseterScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReseterScript : MonoBehaviour
+{
+
+    public float resetDistance = 40;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        other.GetComponent<Transform>().
+        transform.parent.Translate(new Vector3(0, 0, -resetDistance));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/ScoreController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreController : MonoBehaviour
+{
+    public int score = 0;
+    private TMPro.TMP_Text text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<TMPro.TMP_Text>();
+        text.SetText("Score: " + score.ToString());
+    }
+
+    public void UpdateScore(int amount) {
+        score += amount;
+        text.SetText("Score: " + score.ToString());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
