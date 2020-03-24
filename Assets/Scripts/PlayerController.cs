54e3bea3e4e9aae4d29244598d387eb6081bcd9b
andrei2d <andrei.tudorache100@gmail.com>
2020-03-24 12:15:49 +0200

    Several modifications

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -30,7 +30,7 @@ public class PlayerController : MonoBehaviour
         {
             // We are grounded, so recalculate
             // move direction directly from axes
-            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
             moveDirection *= speed;
             if (Input.GetButton("Jump"))
             {
