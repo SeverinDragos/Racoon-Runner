aaf64afb901f0062d20bdfd30ea841e39cf22a8e
Andreea Panait <andreeapanait99@gmail.com>
2020-03-31 00:00:34 +0300

    endless platform

---
+++ Assets/OurScripts/PlayerMotor.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour
+{
+    private CharacterController controller;
+    private float speed = 5.0f;
+    private Vector3 moveVector;
+    private float verticalVelocity = 0.0f;
+    private float gravity = 12.0f;
+    private float animationDuration = 3.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time < animationDuration)
+        {
+            controller.Move(Vector3.forward * speed * Time.deltaTime);
+            return;
+        }
+
+        moveVector = Vector3.zero;
+
+        if (controller.isGrounded)
+        {
+            verticalVelocity = -0.5f;
+        }
+        else
+        {
+            verticalVelocity -= gravity * Time.deltaTime;
+        }
+
+        // X
+        moveVector.x = Input.GetAxisRaw("Horizontal") * speed;
+
+        // Y
+        moveVector.y = verticalVelocity;
+
+        // Z
+        moveVector.z = speed;
+
+        controller.Move(moveVector * Time.deltaTime);
+    }
+}
---
+++ Assets/OurScripts/TileManager.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+    public GameObject[] tilePrefabs;
+    private Transform playerTransform;
+    private float spawnZ = -9.0f;
+    private float tileLength = 9.0f;
+    private int amnTilesOnScreen = 7;
+    private List<GameObject> activeTiles;
+    private float safeZone = 15.0f;
+    private int lastPrefabIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        activeTiles = new List<GameObject>();
+        for (int i = 0; i < amnTilesOnScreen; i++)
+        {
+            if (i < 4)
+            {
+                SpawnTile(0);
+            }
+            else
+            {
+                SpawnTile();
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerTransform.position.z - safeZone > spawnZ - amnTilesOnScreen * tileLength)
+        {
+            SpawnTile();
+            DeleteTile();
+        }
+    }
+
+    private void SpawnTile(int prefabIndex = -1)
+    {
+        GameObject go;
+        if (prefabIndex == -1)
+        {
+            go = Instantiate(tilePrefabs[randomPrefabIndex()]) as GameObject;
+        }
+        else
+        {
+            go = Instantiate(tilePrefabs[prefabIndex]) as GameObject;
+        }
+        go.transform.SetParent(transform);
+        go.transform.position = Vector3.forward * spawnZ;
+        spawnZ += tileLength;
+        activeTiles.Add(go);
+    }
+
+    private void DeleteTile()
+    {
+        Destroy(activeTiles[0]);
+        activeTiles.RemoveAt(0);
+    }
+
+    private int randomPrefabIndex()
+    {
+        if (tilePrefabs.Length <= 1)
+        {
+            return 0;
+        }
+
+        int randomIndex = lastPrefabIndex;
+        while (randomIndex == lastPrefabIndex)
+        {
+            randomIndex = Random.Range(0, tilePrefabs.Length);
+        }
+        lastPrefabIndex = randomIndex;
+        return randomIndex;
+    }
+}
